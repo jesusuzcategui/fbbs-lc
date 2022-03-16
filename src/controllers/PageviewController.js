a174b46fffa8947f90caa342020be1fe33c2bdcd
Jesus Uzcategui <uzcateguijesusdev@gmail.com>
2022-03-15 20:14:09 -0500

    Ended track

--- src/controllers/PageviewController.js
+++ src/controllers/PageviewController.js
@@ -4,37 +4,32 @@ const moment = require('moment');
 const { PromiseHandler } = require('../helper');
 
 const PageviewController = PromiseHandler(async (req, res, next) => {
-    const token = "EAAFzVMai9U4BAPXO0TEroDxPadOCYCMnjNZC02QLNkqTPZBrepRZCfd59nj6k6JHw39m4gI3CDe8CmpPMOMoZBf5NUccfEitQ4065CmfE8BkxMjcBZAJ1quz7wFGHZBvZAncCpmtEnSZBG3cTEsdHLKLZA5VZAZBU5Pl76cvb2eFbQ2bX2lql9ECUuzzoDX2FI6pZCMZD";
-    const pixel = "447222056213334";
-    const version = "v13.0";
+    const token = req.headers['access_fb_token'];
+    const pixel = req.params.pixelId;
+    const version = req.params.version;
 
     let UriEvent = `https://graph.facebook.com/${version}/${pixel}/events?access_token=${token}`;
-
-    console.log(UriEvent);
-
-    let myStartDate = moment().subtract(1, 'm').format();
 
     const params = {
         "data": [
             {
                 "event_name": "ViewContent",
-                "event_time": myStartDate,
+                "event_time": moment().subtract(1, 'm').format(),
                 "event_id": "event.id.123",
-                "event_source_url": "https://comprapindev.tk",
-                "user_data": { "client_ip_address": "186.119.104.71", "client_user_agent": "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/99.0.4844.51 Safari/537.36" }
+                "event_source_url": req.body.url,
+                "user_data": { 
+                    "client_ip_address": req.headers['x-forwarded-for'] || req.socket.remoteAddress, 
+                    "client_user_agent": req.get('User-Agent')
+                }
             }
         ]
-    }
-
-    console.log(params);
+    };
 
     try {
         const { data, status } = await axios.post(UriEvent, params);
-        console.log("data", data, status);
         res.json(data);
     } catch (error) {
-        console.log("", error);
-        res.json({error: error});
+        res.json({ error: error }).status(400);
     }
 
 });
--- src/routes/index.js
+++ src/routes/index.js
@@ -1,5 +1,45 @@
 const PageviewController = require('../controllers/PageviewController');
+const { PromiseHandler } = require('../helper');
+const axios = require('axios');
+const moment = require('moment');
 
 module.exports = (app) => {
-    app.get('/v1/graph-facebook/receive', PageviewController);
+    app.post('/v1/graph-facebook/receive/:pixelId/:version', PageviewController);
+    app.get('/v1/graph-facebook/capture', PruebaGET);
 };
+
+const PruebaGET = PromiseHandler(async (req, res, next) => {
+    const token = "EAAFzVMai9U4BACSVFZAGjptOKAArVKihRSeHh3y6hO4LROYWq0tt5ZCKzGz6tPfbmii335umAPGwe4lZAn1PPQGrvy9ZCSDymtseE7bUr5Vfc3nPL2prUaSS8ai2mEpuE3VJq41JQxhBZAHJU9SUF1Afh1aZAsVVd00RmFvyPZA0HLWYwZBH39VZCQ9f8vXsMsqcZD";
+    const pixel = "447222056213334";
+    const version = "v13.0";
+
+    let UriEvent = `https://graph.facebook.com/${version}/${pixel}/events?access_token=${token}`;
+
+    console.log(UriEvent);
+
+    let myStartDate = moment().subtract(1, 'm').format();
+
+    const params = {
+        "data": [
+            {
+                "event_name": "ViewContent",
+                "event_time": myStartDate,
+                "event_id": "event.id.123",
+                "event_source_url": "https://comprapindev.tk",
+                "user_data": { "client_ip_address": "186.119.104.71", "client_user_agent": "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/99.0.4844.51 Safari/537.36" }
+            }
+        ]
+    }
+
+    console.log(params);
+
+    try {
+        const { data, status } = await axios.post(UriEvent, params);
+        console.log("data", data, status);
+        res.json(data);
+    } catch (error) {
+        console.log("", error);
+        res.json({error: error});
+    }
+
+});
